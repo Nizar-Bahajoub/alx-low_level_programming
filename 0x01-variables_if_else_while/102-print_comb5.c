044181d11a0e944cd457ad0d19b02b0814e81527
nizar <n.bahajoub@gmail.com>
2023-03-18 02:49:30 +0000

    Advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - print number 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, k, l, m, op1, op2;
+
+	i = k = l = m = 48;
+	while (m < 58)
+	{
+		l = 48;
+		while (l < 58)
+		{
+			k = 48;
+			while (k < 58)
+			{
+				i = 48;
+				while (i < 58)
+				{
+					op1 = (h * 10) + l;
+					op2 = (k * 10) +i;
+					if (op1 < op2)
+					{
+						putchar(m);
+						putchar(l);
+						putchar(' ');
+						putchar(k);
+						putchar(i);
+						if (m == 57 && l == 56 && k == 57 && i == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+					i++;
+				}
+				k++;
+			}
+			l++;
+		}
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}

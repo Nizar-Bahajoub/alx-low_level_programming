5514a2ea3b44794685b63242b3bd88c8ed31e7f7
nizar <n.bahajoub@gmail.com>
2023-03-18 02:01:06 +0000

    Advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,13 +18,11 @@ int main(void)
 			{
 				putchar(i);
 				putchar(k);
+				if (k == 9)
+					break;
+				putchar(',');                                                                      putchar(' '); 
 			}
 		}
-		if (k == 9)
-			break;
-		putchar(',');
-		putchar(' ');
-		i++;
 	}
 	putchar('\n');
 	return (0);

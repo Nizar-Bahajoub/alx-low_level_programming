825a7660d6b1e704543645954b57597561906643
nizar <n.bahajoub@gmail.com>
2023-03-18 01:31:05 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print number 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48, c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

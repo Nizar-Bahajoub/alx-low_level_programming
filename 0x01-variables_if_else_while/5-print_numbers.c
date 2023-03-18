c1db1bbf0a192a4c6457bc86dca2b26eff2c8a03
nizar <n.bahajoub@gmail.com>
2023-03-18 00:48:28 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d",i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

213aaa678ef29952707737c889853b8bfbbf854e
nizar <n.bahajoub@gmail.com>
2023-03-18 00:25:28 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/** 
+ * main - printing the alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i = 'a';
+	while (i <= 'z' && i != 'q' && i !='e')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

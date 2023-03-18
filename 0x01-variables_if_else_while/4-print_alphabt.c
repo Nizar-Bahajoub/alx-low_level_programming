fca4433b4cf0ecfd6264ea99f75575a49af7f7bc
nizar <n.bahajoub@gmail.com>
2023-03-18 00:30:58 +0000

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - printing the alphabet
  *
  * Return: 0
@@ -9,9 +9,11 @@
 int main(void)
 {
 	char i = 'a';
-	while (i <= 'z' && i != 'q' && i !='e')
+
+	while (i <= 'z' && i != 'q' && i != 'e')
 	{
 		putchar(i);
+		i++;
 	}
 	putchar('\n');
 	return (0);

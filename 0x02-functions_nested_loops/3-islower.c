c10dc7bbdb3fed12b9ae3889c15a5780928840d2
nizar <n.bahajoub@gmail.com>
2023-03-21 21:50:13 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - cheks for lowercase character
+ *
+ * written by nizar
+ * Return: 1 if c is lower  otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

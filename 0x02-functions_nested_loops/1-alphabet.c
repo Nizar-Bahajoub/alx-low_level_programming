bfbe58d774e62f8d0ee3198eef9503656476a5c9
nizar <n.bahajoub@gmail.com>
2023-03-21 20:56:28 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ * written by nizar
+ * Return: nothing
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	char c = 97;
+
+	for(i = 0; i <= 25; i++)
+	{
+		_putchar('c');
+		c += i;
+	}
+}

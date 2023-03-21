1401f3fcde2944a2336f4d3293b78f0a8432f1c2
nizar <n.bahajoub@gmail.com>
2023-03-21 21:20:41 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lowercase ten times
+ *
+ * written by nizar
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 97;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 97; c <= 122; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}

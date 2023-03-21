7a182c97ef3fb1b5812142dc111d4d7c7562af82
nizar <n.bahajoub@gmail.com>
2023-03-21 23:44:44 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0 ;j < 10; j++)
+		for (j = 0 ; j < 10; j++)
 		{
 			p = i * j;
 			if (p > 9)
@@ -27,7 +27,7 @@ void times_table(void)
 			else if (p != 0)
 			{
 				_putchar(',');
-				_putchar(' ');
+				_putchar('  ');
 				_putchar(p + '0');
 			}
 			else

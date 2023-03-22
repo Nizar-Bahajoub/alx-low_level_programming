2196ec6ba58f6fe90dab92c2295d8e54b7cd73bf
nizar <n.bahajoub@gmail.com>
2023-03-22 02:14:34 +0000

    ADVANCED

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((p / 100) + '0');
-					_putchar(((p - (p % 10)) / 10) % 10 + '0');
+					_putchar((p - (p % 10)) / 10 + '0');
 					_putchar((p % 10) + '0');
 				}
 				else if (p > 99)

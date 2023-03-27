d1a46bf0f3ffc2f2b8ea5a80ff36ef05bfe3e252
nizar <n.bahajou@gmail.com>
2023-03-27 10:37:21 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98 - change value of n to 98
+ *@n: number adress to change
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

54e61f8111999f7460605eb62d210b27c94973e5
nizar <n.bahajoub@gmail.com>
2023-03-28 01:14:13 +0000

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,15 @@
 
 void print_rev(char *s)
 {
-	int i;
-	int len;
+	int i = 0;
+	int len = 0;
 
-	len = _strlen(s);
-	for (i = len - 1; i >= 0; i--)
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	for (i = len; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}

1cb3d1cc833645763d2461edfe4f32d712d53dfc
nizar <n.bahajou@gmail.com>
2023-03-27 17:25:38 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print the string
+ * @str: the string to be print
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

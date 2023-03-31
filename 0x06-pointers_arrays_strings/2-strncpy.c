581986a5e4a1152c7daded4e8514e86945c03625
nizar <n.bahajoub@gmail.com>
2023-03-31 23:24:13 +0000

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string  to change
+ * @src: string source
+ * @n: number of character toadd to dest
+ *
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *(src + i) != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}

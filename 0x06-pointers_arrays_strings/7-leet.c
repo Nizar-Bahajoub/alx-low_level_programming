d1bf044f470664ed6427ae5bbbaa64a94b33ab53
nizar <n.bahajoub@gmail.com>
2023-04-01 01:37:32 +0000

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,13 +7,13 @@
  * Return: str
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
-	int i,j;
+	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; str[i] !='\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{

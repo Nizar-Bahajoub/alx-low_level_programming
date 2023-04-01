eabde0a84f18fa70d6276e6bf70ac79cd4985d31
nizar <n.bahajoub@gmail.com>
2023-04-01 22:01:16 +0000

    Print integer with putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,17 @@
 
 void print_number(int n)
 {
-	char s;
-	int i = 1, j = 0, k;
+	int k;
 
 	k = n;
 	if (n < 0)
 	{
-		_putchr('-');
-		n = -n;
+		_putchar('-');
+		k = -n;
 	}
-	if (n / 10 != 0)
+	if (k / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(k / 10);
 	}
 	_putchar((k % 10) + '0');
 }

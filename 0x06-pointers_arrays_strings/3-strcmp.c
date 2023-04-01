6ca45f0f63a0b3ec8fd3a17c7ad1a4dc20f0d1a9
nizar <n.bahajoub@gmail.com>
2023-04-01 00:05:45 +0000

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,43 +5,20 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return: 0 or 1 or -1
+ * Return: s1[i] - s2[i]
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, l1 = 0, l2 = 0, corr = 0;
+	int i = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		l1++;
-	}
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		l2++;
-	}
-	if (l1 < l2)
-		corr--;
-	else if (l1 > l2)
-		corr++;
-	else
-	{
-		corr = 0;
-		for (i = 0; i < l1; i++)
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] == s2[i])
-				corr++;
+			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
-	if (corr == l1)
-		return (0);
-	else if (corr > 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (0);
 }

9d0ba9a48c551aa239a7b45807a4cc1cac74190d
nizar <n.bahajoub@gmail.com>
2023-04-01 00:59:35 +0000

    Task 5 and 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalization
+ * @str: string to be capitalized
+ *
+ * Return: pointer to str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ( str[i - 1] == ' ' || str [i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+}

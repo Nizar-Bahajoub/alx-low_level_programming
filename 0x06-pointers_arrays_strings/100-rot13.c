e852a46d2b519e8af6bdde15f9e8d752b70839a3
nizar <n.bahajoub@gmail.com>
2023-04-01 20:30:57 +0000

    Advanced tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *rot13 - encode a string using rot13
+ * @str: string encodes
+ *
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+	int i = 0, j = 0;
+	char s1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char s2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZ";
+
+	for (i; str[i] != '\0'; i++)
+	{
+		for (j; s1[j] != '\0'; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}

a81ae37c6b464217fa375d77c16d901af16ba9fe
nizar <n.bahajoub@gmail.com>
2023-04-01 01:32:46 +0000

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - endcode to 1337
+ * @str: input
+ *
+ * Return: str
+ */
+
+char *leet(char *)
+{
+	int i,j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] !='\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}

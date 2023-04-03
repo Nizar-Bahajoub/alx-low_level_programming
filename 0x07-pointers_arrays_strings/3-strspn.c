30d16263dfbf18bf2292a64f5ec06b6454292bd6
nizar <n.bahajoub@gmail.com>
2023-04-03 22:45:48 +0000

    Task 4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,16 +14,18 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] != ' ')
+		{
 			for (j = 0; accept[j] != '\0'; j++)
 			{
 				if (s[i] == accept[j])
 				{
 					c++;
-					break;
 				}
-				else if (accept[j + 1] =='\0')
-					return (c);
 			}
+		}
+		else
+			return (c);
 	}
 	return (c);
 }

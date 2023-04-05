8a87e924e12d881f8250a47db8df2f0e57277c49
nizar <n.bahajoub@gmail.com>
2023-04-05 23:33:39 +0000

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * My_sqrt -  returns the natural square root of a number
+ * @a: the number to be squared
+ * @b: indice
+ *
+ * Return: the n squared
+ */
+
+int My_sqrt(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	else
+		return My_sqrt(a, b +1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a numbrer
+ * @n: the input
+ *
+ * Return: sqrt(n)
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (My_sqrt(n, 0));
+}

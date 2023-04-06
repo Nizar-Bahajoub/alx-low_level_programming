bb1e006c923c49091944b184ed88134d4614ca49
nizar <n.bahajoub@gmail.com>
2023-04-06 01:24:55 +0000

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,7 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return : 0
+ * Return : 0 or 1
  */
 
 int wildcmp(char *s1, char *s2)
@@ -24,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s1 == *s2)
 	{
-		return (wildcmp(s1 + 1, s2 +1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }

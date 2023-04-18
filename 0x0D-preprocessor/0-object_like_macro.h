e94ca283c802433346027335fb68c3c583d4ca17
nizar <n.bahajoub@gmail.com>
2023-04-18 02:44:38 +0000

    TASK 0 and 1

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _SIMPLE_HEADER_H_
+#define _SIMPLE_HEADER_H_
+#define SIZE 1024
+#endif /* _SIMPLE_HEADER_H_ */
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,4 @@
+#ifndef _SIMPLE_HEADER_H_
+#define _SIMPLE_HEADER_H_
+#define PI 3.14159265359
+#endif

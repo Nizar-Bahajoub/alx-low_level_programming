7c32a336fad270a82705b673d1ac065cdef2b2a1
Nizar <n.bahajoub@gmail.com>
2023-04-28 14:49:45 -0700

    Task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print - print a string before the main
+ *
+ * Return: nothing
+ */
+
+void __attribute__((constructor)) print()
+{
+	printf("You\'re beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

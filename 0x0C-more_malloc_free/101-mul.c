04e4d4183fa5c94ae3cfe8d4a718260dd23f4d7b
Nizar <n.bahajoub@gmail.com>
2023-04-28 19:28:00 -0700

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,55 +2,6 @@
 #include <stdlib.h>
 
 /**
- * is_digit - if anumber is digit
- * @str: input
- *
- * Return: int
- */
-int is_digit(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * str_to_int - string to int
- * @str: input
- *
- * Return: int
- */
-int str_to_int(char *str)
-{
-	int n = 0;
-	int i = 0;
-
-	while (str[i])
-	{
-		n *= 10;
-		n += str[i] - '0';
-		i++;
-	}
-	return (n);
-}
-
-/**
- * error - print Error
- *
- * Return: void
- */
-void error(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-/**
  * main - the main function
  * @argc: input
  * @argv: input
@@ -60,19 +11,28 @@ void error(void)
 
 int main(int argc, char **argv)
 {
-	int num1, num2, prod;
+	unsigned long prod;
+	int i, j;
 
 	if (argc != 3)
-		error();
-	if (!is_digit(argv[1]) || !is_digit(argv[2]))
-		error();
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	prod = atol(argv[1]) * atol(argv[2]);
 
-	num1 = str_to_int(argv[1]);
-	num2 = str_to_int(argv[2]);
-
-	prod = num1 * num2;
-
-	printf("%d\n", prod);
+	printf("%lu\n", prod);
 
 	return (0);
 

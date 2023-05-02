45000c152ebfeccc9b35b0842eb370ff1ea33e4f
Nizar <n.bahajoub@gmail.com>
2023-05-02 08:22:27 -0700

    Task 9 updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,35 +17,36 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	unsigned int len = 0, i;
 
-	if (head == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	if (idx == 0)
+	if (head != NULL && *head != NULL)
 	{
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		if (idx == 0)
+		{
+			new->n = n;
+			new->next = *head;
+			*head = new;
+			return (new);
+		}
+		p = *head;
+		while (p != NULL)
+		{
+			p = p->next;
+			len++;
+		}
+		if (idx >= len)
+		{
+			free(new);
+			return (NULL);
+		}
+		p = *head;
+		for (i = 0; i < idx - 1; i++)
+			p = p->next;
+		new->next = p->next;
+		p->next = new;
 		new->n = n;
-		new->next = *head;
-		*head = new;
 		return (new);
 	}
-	p = *head;
-	while (p != NULL)
-	{
-		p = p->next;
-		len++;
-	}
-	if (idx >= len)
-	{
-		free(new);
-		return (NULL);
-	}
-	p = *head;
-	for (i = 0; i < idx - 1; i++)
-		p = p->next;
-	new->next = p->next;
-	p->next = new;
-	new->n = n;
-	return (new);
+	return (NULL);
 }

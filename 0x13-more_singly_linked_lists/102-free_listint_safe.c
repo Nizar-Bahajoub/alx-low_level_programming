23a43085a7a796bca17ad0e5e6b32ec9629a1cfe
Nizar <n.bahajoub@gmail.com>
2023-05-02 18:49:32 -0700

    Task 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a list
+ * @h: the head of the list
+ *
+ * Return: size of the list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *p, *next;
+	unsigned int len = 0;
+
+	if (!h || !*h)
+		return (0);
+	p = *h;
+	while (p != NULL)
+	{
+		len++;
+		next = p;
+		p = p->next;
+		free(next);;
+		if (next <= p)
+			break;
+	}
+	*h = NULL;
+	return (len);
+
+}
